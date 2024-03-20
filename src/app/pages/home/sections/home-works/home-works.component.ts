4f5575bb2bb3c958de53f1425f487f78a87fd96b
yogeshPMishra <mail.yogeshpmishra@gmail.com>
2024-03-20 12:27:27 +0530

    inner pages

---
+++ src/app/pages/home/sections/home-works/home-works.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-works',
+  templateUrl: './home-works.component.html',
+  styleUrls: ['./home-works.component.scss']
+})
+export class HomeWorksComponent {
+
+}
